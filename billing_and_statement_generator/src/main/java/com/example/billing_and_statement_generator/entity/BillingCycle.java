db00eface9189bcb31d514cf22cb21a54aee178b
Divij Jain <129066756+Divij-J@users.noreply.github.com>
2026-03-05 16:46:37 -0600

    Entities for Customer and BillingCycle tables

---
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/entity/BillingCycle.java
@@ -0,0 +1,63 @@
+package com.example.billing_and_statement_generator.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+@Entity
+@Table(name="billing_cycle")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BillingCycle {
+    @Id
+    @Column(name = "cycle_id", nullable = false)
+    private UUID cycle_id;
+    //@ManyToOne
+    //@Joincolumn(name = "card_id")
+    //private Card card;       //FK to cardid
+
+    @Column(name = "cycle_start_date")
+    private LocalDate cycle_start_date;
+
+    @Column(name = "cycle_end_date")
+    private LocalDate cycle_end_date;
+
+    @Column(name = "due_date")
+    private LocalDate due_date;
+
+    @Column(name = "credit_limit", precision= 15, scale = 2)
+    private BigDecimal credit_limit;
+
+    @Column(name = "previous_balance", precision= 15, scale = 2)
+    private BigDecimal previous_balance;
+
+    @Column(name = "total_purchases", precision= 15, scale = 2)
+    private BigDecimal total_purchases;
+
+    @Column(name = "total_cash_advance", precision= 15, scale = 2)
+    private BigDecimal total_cash_advance;
+
+    @Column(name = "total_interest", precision= 15, scale = 2)
+    private BigDecimal total_interest;
+
+    @Column(name = "total_outstanding", precision= 15, scale = 2)
+    private BigDecimal total_outstanding;
+
+    @Column(name = "minimum_due", precision= 15, scale = 2)
+    private BigDecimal minimum_due;
+
+    @Column(name = "cycle_status")
+    private String cycle_status;
+
+    //@OneToMany(mappedBy = "BillingCycle")
+    //private  List<Transaction> transactions;
+}
---
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/entity/Customer.java
@@ -0,0 +1,77 @@
+package com.example.billing_and_statement_generator.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@Entity @Table(name="customer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @Column(name = "customer_id", nullable = false)
+    private UUID customerId;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    @Column(name = "first_name", nullable = false)
+    String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    @Column(name = "last_name", nullable = false)
+    private String lastNname;
+
+    @Size(max = 1)
+    @Column(name = "middle_initial")
+    private String middleInitial;
+
+    @NotBlank
+    @Email
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(name = "phone_number", nullable = false, unique = true,  length = 15)
+    private String phoneNumber;
+
+    @Column(name = "phone_type")
+    private String phonetype;
+
+    @NotBlank
+    @Column(name = "address_line_1", nullable = false)
+    private String address1;
+
+    @Column(name = "address_line_2")
+    private String address2;
+
+    @NotBlank
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, max = 2)
+    @Column(name = "state", nullable = false, length = 2)
+    private String state;
+
+    @NotBlank
+    @Column(name = "zip_code", nullable = false, length = 5)
+    private String zipcode;
+
+    //@OneToMany(mappedBy = "customer")
+    //Private List<Card> cards;
+}
