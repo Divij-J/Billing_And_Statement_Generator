24d4fb715e531f01f924590b0cdd19b568f754dc
sharay2 <sharayo52@gmail.com>
2026-03-07 17:56:16 -0600

    Created DTO's for Cards and Transactions

---
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/dto/CreateTransactionRequestDTO.java
@@ -0,0 +1,41 @@
+package com.example.billing_and_statement_generator.dto;
+
+import com.example.billing_and_statement_generator.entity.Transaction.*;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateTransactionRequestDTO {
+
+    @NotNull
+    private UUID cardId;
+
+    private UUID transactionId;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDate transactionDate;
+
+    @NotNull
+    @DecimalMin("0.01") //transaction is at least $0.01
+    @Digits(integer = 12, fraction = 2)
+    private BigDecimal amount;
+
+    @NotBlank
+    @Size(max = 140)
+    private String merchantName;
+
+    @NotNull
+    private transactionType type;
+
+    @NotNull
+    private Status status;
+
+    }
