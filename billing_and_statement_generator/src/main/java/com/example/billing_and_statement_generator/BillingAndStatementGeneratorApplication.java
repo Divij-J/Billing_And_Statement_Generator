4502810a53596ea646af1e4bde8a229b0324ac8f
Muddapuram <rishika-goud.muddapuram@capgemini.com>
2026-03-12 19:31:55 -0500

    Updated Repositories

--- billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/BillingAndStatementGeneratorApplication.java
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/BillingAndStatementGeneratorApplication.java
@@ -9,5 +9,5 @@ public class BillingAndStatementGeneratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BillingAndStatementGeneratorApplication.class, args);
 	}
-
 }
+
