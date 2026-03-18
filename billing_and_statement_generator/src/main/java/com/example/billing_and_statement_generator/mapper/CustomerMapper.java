59187bcf46b4d433335cb7d9e52cc2be1884ece9
Divij Jain <129066756+Divij-J@users.noreply.github.com>
2026-03-18 16:00:40 -0500

    Fixed Customermapper

--- billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/mapper/CustomerMapper.java
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/mapper/CustomerMapper.java
@@ -6,6 +6,7 @@ import com.example.billing_and_statement_generator.entity.Customer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
