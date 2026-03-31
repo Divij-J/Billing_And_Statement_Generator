192e4e57b5dabf039272a4958bdedf57d1881797
sharay2 <sharayo52@gmail.com>
2026-03-31 13:11:48 -0500

    Refactored endpoints to use DTOs

--- billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/Controller/CardController.java
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/Controller/CardController.java
@@ -1,7 +1,10 @@
 package com.example.billing_and_statement_generator.Controller;
 
-import com.example.billing_and_statement_generator.dto.CreateCardRequestDTO;
-import com.example.billing_and_statement_generator.dto.CreateCardResponseDTO;
+import com.example.billing_and_statement_generator.dto.CustomerIdDTO;
+import com.example.billing_and_statement_generator.dto.card.CreateCardRequestDTO;
+import com.example.billing_and_statement_generator.dto.card.CreateCardResponseDTO;
+import com.example.billing_and_statement_generator.dto.card.CardIdDTO;
+import com.example.billing_and_statement_generator.dto.card.GetCardBalanceResponseDTO;
 import com.example.billing_and_statement_generator.services.CardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,9 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("api/cards")
@@ -23,7 +24,7 @@ public class CardController {
     private final CardService cardService;
 
     // POST: Endpoint for creating new card (v1)
-    @PostMapping("/v1")
+    @PostMapping("/v1/createCard")
     @Operation(summary = "Create a card (V1)",
             description = "Create a card with given information for a customer")
     public ResponseEntity<CreateCardResponseDTO> createCardV1(@Valid @RequestBody CreateCardRequestDTO dto) {
@@ -32,26 +33,27 @@ public class CardController {
     }
 
     // POST: Endpoint for finding information about a specific card
-    @PostMapping("/v1/{cardId}")
+    @PostMapping("/v1/getCardInfo")
     @Operation(summary = "Card Information (V1)",
             description = "Fetch card information from a card ID")
-    public ResponseEntity<CreateCardResponseDTO> getCard(@PathVariable UUID cardId) {
-        return ResponseEntity.ok(cardService.getById(cardId));
+    public ResponseEntity<CreateCardResponseDTO> getCard(@Valid @RequestBody CardIdDTO dto) {
+        CreateCardResponseDTO response = cardService.getById(dto.getCardId());
+        return ResponseEntity.ok(response);
     }
 
     // POST: Endpoint for fetching all cards to a customer
-    @PostMapping("/v1/customer/cards/{customerId}")
-    @Operation(summary = "Fetch All Cards (V1)",
+    @PostMapping("/v1/getCardsByCustomerId")
+    @Operation(summary = "Fetch All Cards By Customer ID (V1)",
             description = "Fetches all cards from a customer ID")
-    public ResponseEntity<List<CreateCardResponseDTO>> getCardsByCustomer(@PathVariable UUID customerId) {
-        return ResponseEntity.ok(cardService.getByCustomer(customerId));
+    public ResponseEntity<List<CreateCardResponseDTO>> getCardsByCustomer(@Valid @RequestBody CustomerIdDTO dto) {
+        return ResponseEntity.ok(cardService.getByCustomer(dto.getCustomerId()));
     }
 
     // POST: Endpoint for fetching card balance for a specific card
-    @PostMapping("/v1/balance/{cardId}")
-    @Operation(summary = "Fetch Card Balance",
+    @PostMapping("/v1/getCardBalanceByCardId")
+    @Operation(summary = "Fetch Card Balance by Card ID (V1)",
             description = "Fetches card balance for a specific card")
-    public ResponseEntity<BigDecimal> getBalance(@PathVariable UUID cardId) {
-        return ResponseEntity.ok(cardService.getTotalBalance(cardId));
+    public ResponseEntity<GetCardBalanceResponseDTO> getBalance(@Valid @RequestBody CardIdDTO dto) {
+        return ResponseEntity.ok(cardService.getBalances(dto.getCardId()));
     }
 }
--- billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/Controller/TransactionController.java
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/Controller/TransactionController.java
@@ -1,7 +1,8 @@
 package com.example.billing_and_statement_generator.Controller;
 
-import com.example.billing_and_statement_generator.dto.CreateTransactionRequestDTO;
-import com.example.billing_and_statement_generator.dto.CreateTransactionResponseDTO;
+import com.example.billing_and_statement_generator.dto.BillingCycleIdDTO;
+import com.example.billing_and_statement_generator.dto.card.CardIdDTO;
+import com.example.billing_and_statement_generator.dto.transaction.*;
 import com.example.billing_and_statement_generator.services.TransactionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,9 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -23,7 +22,7 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     // POST: Endpoint for creating a transaction (purchase, cash advance, payment)
-    @PostMapping("/v1")
+    @PostMapping("/v1/createTransaction")
     @Operation(summary = "Create Transaction (V1)",
         description = "Create a Purchase or Cash Advance transaction")
     public ResponseEntity<CreateTransactionResponseDTO> create(@Valid @RequestBody CreateTransactionRequestDTO dto) {
@@ -32,53 +31,46 @@ public class TransactionController {
     }
 
     // POST: Endpoint for fetching a transaction by its ID
-    @PostMapping("/v1/{transactionId}")
+    @PostMapping("/v1/getTransactionById")
     @Operation(summary = "Fetch Transaction (V1)",
         description = "Fetches information about a specific transaction by its ID")
-    public ResponseEntity<CreateTransactionResponseDTO> getById(@PathVariable UUID transactionId) {
-        return ResponseEntity.ok(transactionService.getById(transactionId));
+    public ResponseEntity<CreateTransactionResponseDTO> getById(@Valid @RequestBody TransactionIdDTO dto) {
+        return ResponseEntity.ok(transactionService.getById(dto.getTransactionId()));
     }
 
     // POST: Endpoint for fetching list of transactions by its Card ID
-    @PostMapping("/v1/card/{cardId}")
+    @PostMapping("/v1/getTransactionsByCardId")
     @Operation(summary = "Fetch All Transactions (V1)",
         description = "Fetches all transactions from a card ID")
-    public ResponseEntity<List<CreateTransactionResponseDTO>> listByCard(@PathVariable UUID cardId) {
-        return ResponseEntity.ok(transactionService.listByCard(cardId));
+    public ResponseEntity<List<CreateTransactionResponseDTO>> listByCard(@Valid @RequestBody CardIdDTO dto) {
+        return ResponseEntity.ok(transactionService.listByCard(dto.getCardId()));
     }
 
     // POST: Endpoint for fetching ALL transactions from a certain Billing Cycle ID
-    @PostMapping("/v1/billing-cycle/{cycleId}")
+    @PostMapping("/v1/getTransactionsByBillingCycle")
     @Operation(summary = "Fetch All Transactions by Cycle ID (V1)",
             description = "Fetches all transactions from a Cycle ID")
-    public ResponseEntity<List<CreateTransactionResponseDTO>> listByCycle(@PathVariable UUID cycleId) {
-        return ResponseEntity.ok(transactionService.listByCycle(cycleId));
+    public ResponseEntity<List<CreateTransactionResponseDTO>> listByCycle(@Valid @RequestBody BillingCycleIdDTO dto) {
+        return ResponseEntity.ok(transactionService.listByCycle(dto.getCycleId()));
     }
 
     // POST: Endpoint for fetching transactions based on Card ID and date range
-    @PostMapping("/v1/card/range/{cardId}")
+    @PostMapping("/v1/getTransactionsWithinDateRange")
     @Operation(summary = "Fetch All Transactions by a Date Range (V1)",
             description = "Fetches all transactions with a range of dates")
-    public ResponseEntity<List<CreateTransactionResponseDTO>> listByDateRange(
-            @PathVariable UUID cardId,
-            @RequestParam LocalDate start,
-            @RequestParam LocalDate end
-    ) {
+    public ResponseEntity<List<CreateTransactionResponseDTO>> listByDateRange(@Valid @RequestBody GetTransactionsBetweenDatesRequestDTO dto) {
         return ResponseEntity.ok(
-                transactionService.listByCardAndDateRange(cardId, start, end)
+                transactionService.listByCardAndDateRange(dto.getCardId(), dto.getStartDate(), dto.getEndDate())
         );
     }
 
     // POST: Endpoint for fetching transactions by Card ID and Billing Cycle ID
-    @PostMapping("/v1/card/billing-cycle/{cardId}/{cycleId}")
+    @PostMapping("/v1/getTransactionsByCardAndCycleID")
     @Operation(summary = "Fetch All Transactions by Card and Cycle ID (V1)",
             description = "Fetches all transactions from a Card and Cycle ID")
-    public ResponseEntity<List<CreateTransactionResponseDTO>> listByCardAndCycle(
-            @PathVariable UUID cardId,
-            @PathVariable UUID cycleId
-    ) {
+    public ResponseEntity<List<CreateTransactionResponseDTO>> listByCardAndCycle(@Valid @RequestBody GetTransactionsByCardIdCycleIdRequestDTO dto) {
         return ResponseEntity.ok(
-                transactionService.listByCardAndBillingCycle(cardId, cycleId)
+                transactionService.listByCardAndBillingCycle(dto.getCardId(), dto.getCycleId())
         );
     }
 }
