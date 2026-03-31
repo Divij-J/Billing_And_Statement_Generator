0866f62454b23bbf89aff21f7013f1e8987a13c9
sharay2 <sharayo52@gmail.com>
2026-03-31 13:19:09 -0500

    Moved DTOs to its categories

--- billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/dto/card/CreateCardRequestDTO.java
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/dto/card/CreateCardRequestDTO.java
@@ -1,4 +1,4 @@
-package com.example.billing_and_statement_generator.dto;
+package com.example.billing_and_statement_generator.dto.card;
 
 import com.example.billing_and_statement_generator.entity.Card.CardType;
 import jakarta.validation.constraints.*;
@@ -11,21 +11,21 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class CreateCardRequestDTO {
-    @NotNull
+    @NotNull(message = "Card ID is required")
     private UUID customerId;
 
-    @NotBlank
+    @NotBlank(message = "Card Number is required")
     @Size(min = 12, max = 19)
     @Pattern(regexp = "^[0-9]+$", message = "cardNumber must be digits only")
     private String cardNumber;
 
-    @NotNull
+    @NotNull(message = "Card Type is required")
     private CardType cardType;
 
-    @NotBlank
+    @NotBlank(message = "Card Holder Name is required")
     private String cardHolderName;
 
-    @NotBlank
+    @NotBlank(message = "Security Code is required")
     @Size(min = 3, max = 4)
     @Pattern(regexp = "^[0-9]+$", message = "securityCode must be digits only")
     private String securityCode;
--- billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/dto/transaction/CreateTransactionResponseDTO.java
+++ billing_and_statement_generator/src/main/java/com/example/billing_and_statement_generator/dto/transaction/CreateTransactionResponseDTO.java
@@ -1,4 +1,4 @@
-package com.example.billing_and_statement_generator.dto;
+package com.example.billing_and_statement_generator.dto.transaction;
 
 import com.example.billing_and_statement_generator.entity.Transaction.*;
 
